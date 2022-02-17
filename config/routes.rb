8db531e7c39f8ab52429dd86f0f32b42b4c64261
randallwstanford <randallwstanford@gmail.com>
2022-02-17 15:31:45 -0700

    Creates logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: false
-
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
 
   root to: 'main#index'
 end
