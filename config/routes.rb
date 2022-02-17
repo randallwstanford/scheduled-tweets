c02e6854713e6c64389fbfaa6e665633d3ab3f7b
randallwstanford <randallwstanford@gmail.com>
2022-02-17 15:49:19 -0700

    creates sign in route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'
 
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
   root to: 'main#index'
 end
