41859586dde2f46440672d2cf9f9f45ccb7338aa
randallwstanford <randallwstanford@gmail.com>
2022-02-18 11:17:41 -0700

    Creates update, edit, and passwords_params

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,25 @@ class PasswordResetsController < ApplicationController
 
     redirect_to root_path, notice: 'If there is an account with that email, we have sent a link to reset the password.'
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Your token has expired, please try again.'
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: 'Your password was reset successfully. Please sign in.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
