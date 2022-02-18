258446f083000f46050ae96be0a1a92140aecd4c
randallwstanford <randallwstanford@gmail.com>
2022-02-18 11:15:24 -0700

    Changes password/reset/edit#update from post to patch

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   post 'password/reset', to: 'password_resets#create'
 
   get 'password/reset/edit', to: 'password_resets#edit'
-  post 'password/reset/edit', to: 'password_resets#update'
+  patch 'password/reset/edit', to: 'password_resets#update'
 
   root to: 'main#index'
 end
