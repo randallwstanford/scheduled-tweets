85e6076d18ac53ee169ad0a0b2938aed6e70d804
randallwstanford <randallwstanford@gmail.com>
2022-02-20 14:16:52 -0700

    Creates association of account to twitter accounts

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: 'Success'
+    Rails.logger.info auth
+    Current.user.twitter_accounts.create(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.info.token,
+      secret: auth.info.secret
+    )
+    redirect_to root_path, notice: 'Successfully connected your account'
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
