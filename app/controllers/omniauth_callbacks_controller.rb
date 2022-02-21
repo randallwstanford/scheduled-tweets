fb29ec2b39a616f684b7536d69367c1158cc70c3
randallwstanford <randallwstanford@gmail.com>
2022-02-21 11:10:59 -0700

    Only adds twitter account if not associated or added yet.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,14 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    Rails.logger.info auth
-    Current.user.twitter_accounts.create(
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+
+    twitter_account.update(
       name: auth.info.name,
-      username: auth.info.nickname,
       image: auth.info.image,
-      token: auth.info.token,
-      secret: auth.info.secret
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
     )
+
     redirect_to root_path, notice: 'Successfully connected your account'
   end
 
