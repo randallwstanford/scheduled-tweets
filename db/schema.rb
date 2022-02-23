c465b5bff0426a049a22627668975016ae007890
randallwstanford <randallwstanford@gmail.com>
2022-02-23 12:44:41 -0700

    Removes comments

--- db/schema.rb
+++ db/schema.rb
@@ -1,48 +1,36 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_171401) do
-  create_table "tweets", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "twitter_account_id", null: false
-    t.text "body"
-    t.datetime "publish_at"
-    t.string "tweet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["twitter_account_id"], name: "index_tweets_on_twitter_account_id"
-    t.index ["user_id"], name: "index_tweets_on_user_id"
+ActiveRecord::Schema[7.0].define(version: 20_220_222_171_401) do
+  create_table 'tweets', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'twitter_account_id', null: false
+    t.text 'body'
+    t.datetime 'publish_at'
+    t.string 'tweet_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['twitter_account_id'], name: 'index_tweets_on_twitter_account_id'
+    t.index ['user_id'], name: 'index_tweets_on_user_id'
   end
 
-  create_table "twitter_accounts", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "name"
-    t.string "username"
-    t.string "image"
-    t.string "token"
-    t.string "secret"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
+  create_table 'twitter_accounts', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.string 'name'
+    t.string 'username'
+    t.string 'image'
+    t.string 'token'
+    t.string 'secret'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_twitter_accounts_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'password_digest'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "tweets", "twitter_accounts"
-  add_foreign_key "tweets", "users"
-  add_foreign_key "twitter_accounts", "users"
+  add_foreign_key 'tweets', 'twitter_accounts'
+  add_foreign_key 'tweets', 'users'
+  add_foreign_key 'twitter_accounts', 'users'
 end
